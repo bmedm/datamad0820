06f3d3dd8c17207f01c58c924a7cabb0d193e692
bmedm <b.medialm@hotmail.es>
2020-08-20 20:47:38 +0200

    Repasar bonus

--- module-1/lab-errhand_listcomp/your-code/main.py
+++ module-1/lab-errhand_listcomp/your-code/main.py
@@ -1,3 +1,7 @@
+import random
+import os
+import sys
+
 #Example: 
 
 eggs = (1,3,8,3,2)
@@ -14,83 +18,82 @@ print(my_listComprehension)
 #1. Calculate the square number of the first 20 numbers. Use square as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-
-
+square=[i**2 for i in range(21)]
+print(square)
 
 #2. Calculate the first 50 power of two. Use power_of_two as the name of the list.
 # Remember to use list comprehensions and to print your results
-
-
-
+power_of_two=[2**pot for pot in range(50)]
+print(power_of_two)
 
 #3. Calculate the square root of the first 100 numbers. Use sqrt as the name of the list.
 # You will probably need to install math library with pip and import it in this file.  
 # Remember to use list comprehensions and to print your results
 
-
-
+sqrt=[i**(1/2) for i in range(101)]
+print(sqrt)
 
 #4. Create this list [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0]. Use my_list as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-
-
+my_list=[i for i in range(-10,1)]
+print(my_list)
 
 #5. Find the odd numbers from 1-100. Use odds as the name of the list. 
 # Remember to use list comprehensions and to print your results
 
-
-
+odds=[i for i in range(1,101) if i%2==1]
+print(odds)
 
 #6. Find all of the numbers from 1-1000 that are divisible by 7. Use divisible_by_seven as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-
-
+divisible_by_seven=[i for i in range(1,1001) if not i%7]
+print(divisible_by_seven)
 
 #7. Remove all of the vowels in a string. Hint: make a list of the non-vowels. Use non_vowels as the name of the list.
 # Remember to use list comprehensions and to print your results
 # You can use the following test string but feel free to modify at your convenience
 
 teststring = 'Find all of the words in a string that are monosyllabic'
-
-
-
+non_vowels="".join([i for i in teststring if i not in ["a","e","i","o","u"]])
+print(non_vowels)
 
 #8. Find the capital letters (and not white space) in the sentence 'The Quick Brown Fox Jumped Over The Lazy Dog'. 
 # Use capital_letters as the name of the list.  
 # Remember to use list comprehensions and to print your results
 
-
-
+sentence='The Quick Brown Fox Jumped Over The Lazy Dog'
+capital_letters=[capital for capital in sentence if capital== capital.upper()]
+print(capital_letters)
 
 #9. Find all the consonants in the sentence 'The quick brown fox jumped over the lazy dog'.
 # Use consonants as the name of the list.
 # Remember to use list comprehensions and to print your results.
 
-
-
-
+consonants=[[i for i in sentence if i not in ["a","e","i","o","u"] and i != " "]]
+print(consonants)
 
 #10. Find the folders you have in your madrid-oct-2018 local repo. Use files as name of the list.  
 # You will probably need to import os library and some of its modules. You will need to make some online research.
 # Remember to use list comprehensions and to print your results.
 
-
+files=[x for x in os.listdir('/Users/beatrizmedialdeamontes/github/datamad0820')]
+print(files)
 
 #11. Create 4 lists of 10 random numbers between 0 and 100 each. Use random_lists as the name of the list. 
 #You will probably need to import random module
 # Remember to use list comprehensions and to print your results
 
-
-
+random_list=[[random.randint(0,100) for i in range(10)]for x in range(4)]
+print(random_list)
 
 #12. Flatten the following list of lists. Use flatten_list as the name of the output.
 # Remember to use list comprehensions and to print your results
 
 list_of_lists = [[1,2,3],[4,5,6],[7,8,9]]
-
-
+flatten_list=[e for i in list_of_lists for e in i]
+print(flatten_list)
 
 #13. Convert the numbers of the following nested list to floats. Use floats as the name of the list. 
 # Remember to use list comprehensions and to print your results.
@@ -99,14 +102,18 @@ list_of_lists = [['40', '20', '10', '30'], ['20', '20', '20', '20', '20', '30', 
 ['30', '20', '30', '50', '10', '30', '20', '20', '20'], ['100', '100'], ['100', '100', '100', '100', '100'], \
 ['100', '100', '100', '100']]
 
-
+floats=[ float(e) for i in list_of_lists for e in i]
+print(floats)
 
 
 #14. Handle the exception thrown by the code below by using try and except blocks. 
 
-
 for i in ['a','b','c']:
-    print i**2
+    try:
+        print (i**2)
+    except:
+        print("No se pueden multiplicar letras ")
+print("Adios")
 
 
 #15. Handle the exception thrown by the code below by using try and except blocks. 
@@ -115,43 +122,57 @@ for i in ['a','b','c']:
 
 x = 5
 y = 0
-
-z = x/y
-
-
-
+try:
+    z = x/y
+except:
+    print("¡No dividas entre 0!")
+finally:
+    print("Mejor cambiamos el divisor")
 
 #16. Handle the exception thrown by the code below by using try and except blocks. 
 # Check in provided resources the type of error you may use. 
 
 abc=[10,20,20]
-print(abc[3])
-
+try:
+    print(abc[3])
+except IndexError:
+    print("El índice no existe")
 
 #17. Handle at least two kind of different exceptions when dividing a couple of numbers provided by the user. 
 # Hint: take a look on python input function. 
 # Check in provided resources the type of error you may use. 
 
+a=int(input("Escribe el número del dividendo (int o float, please!) "))
+b=int(input("Ahora el número del divisor (int o float, y distinto de 0) "))
 
+try:
+    division= a/b
+    print(division)
+except TypeError:
+    print("Por favor, escribe un carácter numérico")
+except ZeroDivisionError:
+    print("No dividas entre 0")
 
 
 #18. Handle the exception thrown by the code below by using try and except blocks. 
 # Check in provided resources the type of error you may use. 
 
-f = open('testfile','r')
-f.write('Test write this')
-
-
+try:
+    f = open('testfile','r')
+    f.write('Test write this')
+except:
+    print("Algo ha ido mal")
 
 
 #19. Handle the exceptions that can be thrown by the code below using try and except blocks. 
 #Hint: the file could not exist and the data could not be convertable to int
 
-fp = open('myfile.txt')
+try:
+    fp = open('myfile.txt')
     line = f.readline()
     i = int(s.strip())
-
-
+except FileNotFoundError:
+    print("No se ha encontrado el archivo")
 
 
 #20. The following function can only run on a Linux system. 
@@ -160,8 +181,14 @@ fp = open('myfile.txt')
 # You will probably need to import sys 
 
 def linux_interaction():
-    assert ('linux' in sys.platform), "Function can only run on Linux systems."
-    print('Doing something.')
+    try:
+        assert ('linux' in sys.platform)
+    except AssertionError:
+        print('"Function can only run on Linux systems. Doing something.')
+
+linux_interaction()
+
+
 
 
 # Bonus Questions:
@@ -171,21 +198,37 @@ def linux_interaction():
 #21.  Write a function that asks for an integer and prints the square of it. 
 # Hint: we need to continually keep checking until we get an integer.
 # Use a while loop with a try,except, else block to account for incorrect inputs.
-
-
+while True:
+    try:
+        a=int(input("Escribe un número "))
+        b=a**2
+        print({f"El cuadrado de {a} es {b}"})
+        break
+    except:
+        print("Algo ha salido mal, vuelve a intentarlo y comprueba que sea un número")
 
 
 # 22. Find all of the numbers from 1-1000 that are divisible by any single digit besides 1 (2-9). 
 # Use results as the name of the list 
 
-
-
+results=[num for num in range(1,1001) if [div for div in range(2,10) if num % div == 0]]
+print(results)
 
 # 23. Define a customised exception to handle not accepted values. 
 # You have the following user inputs and the Num_of_sections can not be less than 2.
 # Hint: Create a class derived from the pre-defined Exception class in Python
 
-Total_Marks = int(input("Enter Total Marks Scored: ")) 
-Num_of_Sections = int(input("Enter Num of Sections: "))
+"""
+class error_gordo(Exception):
+    pass
+try:
+    Total_Marks = int(input("Enter Total Marks Scored: ")) 
+    Num_of_Sections = int(input("Enter Num of Sections: "))
+    if Num_of_Section<2:
+        raise error_gordo
+    raise error_gordo
+except error_gordo:
+    print("Has dado un dato incompatible")
 
+"""
 
