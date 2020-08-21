cacd1d463df10060e4eb0dc57839d94a548d72a8
bmedm <b.medialm@hotmail.es>
2020-08-21 17:46:55 +0200

    Vikings and Saxons

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -17,17 +17,37 @@ class Soldier:
 # Viking
 
 
-class Viking:
+class Viking (Soldier):
+    def __init__(self,name, health, strength):
+        super().__init__(health, strength)
+        self.name = name
+    def receiveDamage (self, damage):
+        self.damage= damage
+        self.health=self.health-damage
+        if self.health==0:
+            return f"{self.name} has died in act of combat"
+        else:
+            return f"{self.name} has received {self.damage} points of damage"    
+    def battleCry(self):
+        return "Odin Owns You All!"
+
     pass
 
 # Saxon
 
-
-class Saxon:
+class Saxon(Soldier):
+    def receiveDamage(self, damage):
+        self.damage= damage
+        self.health=self.health-damage
+        if self.health==0:
+            return f"A Saxon has died in combat"
+        else:
+            return f"A Saxon has received {self.damage} points of damage" 
     pass
 
 # War
 
 
 class War:
+
     pass
