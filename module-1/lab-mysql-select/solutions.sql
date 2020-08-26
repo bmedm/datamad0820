2601d945c592057b3d94d6008b94e8242829288a
bmedm <b.medialm@hotmail.es>
2020-08-26 19:46:19 +0200

    Cambios-2 y solucion1º-4)

--- module-1/lab-mysql-select/solutions.sql
+++ module-1/lab-mysql-select/solutions.sql
@@ -21,7 +21,7 @@ SELECT au.au_id AS Author_ID,
     au.au_lname AS Lname, 
     au.au_fname AS Fname,
     pub.pub_name AS Publisher, 
-    COUNT(*) AS Count_title
+    COUNT(t.title) AS Count_title
 	FROM titles AS t 
     INNER JOIN publishers AS pub
     ON  t.pub_id = pub.pub_id
@@ -29,8 +29,8 @@ SELECT au.au_id AS Author_ID,
     ON  t.title_id = tta.title_id
     INNER JOIN authors AS au
     ON tta.au_id = au.au_id
-    GROUP BY t.title
-    ORDER BY Publisher
+    GROUP BY au.au_id, au.au_lname, au.au_fname,pub.pub_name
+    ORDER BY au.au_lname
 
 # CHALLENGE_3
 
@@ -42,11 +42,24 @@ SELECT au.au_id AS Author_ID, au.au_lname AS Lname, au.au_fname AS Fname, SUM(t.
     ON  t.title_id = tta.title_id
     INNER JOIN authors AS au
     ON tta.au_id = au.au_id
-    GROUP BY au.au_id
+    GROUP BY au.au_id, au.au_lname, au.au_fname
     ORDER BY sum(t.ytd_sales) DESC
     LIMIT 3;
 
 # CHALLENGE_4
+SELECT au.au_id AS Author_ID, au.au_lname AS Lname, au.au_fname AS Fname, SUM(t.ytd_sales) AS TOTAL
+	FROM titles AS t 
+	LEFT JOIN sales
+    ON  t.title_id = sales.title_id
+    RIGHT JOIN titleauthor AS tta
+    ON  t.title_id = tta.title_id
+    RIGHT JOIN authors AS au
+    ON tta.au_id = au.au_id
+    GROUP BY au.au_id, au.au_lname, au.au_fname
+    ORDER BY sum(t.ytd_sales) DESC
+
+# BONUS
+
 
 
 
