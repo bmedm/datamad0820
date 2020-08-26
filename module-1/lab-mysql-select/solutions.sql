28fba0b57383c91307bcfeab029e9ca1106659de
bmedm <b.medialm@hotmail.es>
2020-08-26 18:17:11 +0200

    Cambio 2º C-3

--- module-1/lab-mysql-select/solutions.sql
+++ module-1/lab-mysql-select/solutions.sql
@@ -34,7 +34,7 @@ SELECT au.au_id AS Author_ID,
 
 # CHALLENGE_3
 
-SELECT au.au_id AS Author_ID, au.au_lname AS Lname, au.au_fname AS Fname, COUNT(*) AS total, SUM(t.ytd_sales) AS qty
+SELECT au.au_id AS Author_ID, au.au_lname AS Lname, au.au_fname AS Fname, SUM(t.ytd_sales) AS TOTAL
 	FROM titles AS t 
 	INNER JOIN sales
     ON  t.title_id = sales.title_id
@@ -42,9 +42,11 @@ SELECT au.au_id AS Author_ID, au.au_lname AS Lname, au.au_fname AS Fname, COUNT(
     ON  t.title_id = tta.title_id
     INNER JOIN authors AS au
     ON tta.au_id = au.au_id
-    GROUP BY t.title 
-    ORDER BY qty DESC
+    GROUP BY au.au_id
+    ORDER BY sum(t.ytd_sales) DESC
     LIMIT 3;
 
-    # CHALLENGE_4
+# CHALLENGE_4
+
+
 
