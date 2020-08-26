77475516c2789c4ffd42aadaadfcc81f347f50d6
bmedm <b.medialm@hotmail.es>
2020-08-26 23:49:50 +0200

    Borrador de Bonus

--- module-1/lab-mysql-select/solutions.sql
+++ module-1/lab-mysql-select/solutions.sql
@@ -2,8 +2,8 @@
 # CHALLENGE_1
 
 SELECT au.au_id AS Author_ID, 
-    au.au_lname AS Lname, 
-    au.au_fname AS Fname,
+    au.au_lname AS Last_name, 
+    au.au_fname AS First_name,
     t.title AS Title,
     pub.pub_name AS Publisher
 	FROM titles AS t 
@@ -18,10 +18,10 @@ SELECT au.au_id AS Author_ID,
 # CHALLENGE_2
 
  SELECT au.au_id AS Author_ID, 
-    au.au_lname AS Lname, 
-    au.au_fname AS Fname,
+    au.au_lname AS Last_name, 
+    au.au_fname AS First_name,
     pub.pub_name AS Publisher, 
-    COUNT(t.title) AS Count_title
+    COUNT(t.title) AS Count_Title
 	FROM titles AS t 
     INNER JOIN publishers AS pub
     ON  t.pub_id = pub.pub_id
@@ -34,7 +34,10 @@ SELECT au.au_id AS Author_ID,
 
 # CHALLENGE_3
 
-SELECT au.au_id AS Author_ID, au.au_lname AS Lname, au.au_fname AS Fname, SUM(t.ytd_sales) AS TOTAL
+SELECT au.au_id AS Author_ID, 
+    au.au_lname AS Last_name, 
+    au.au_fname AS First_name, 
+    SUM(t.ytd_sales) AS TOTAL
 	FROM titles AS t 
 	INNER JOIN sales
     ON  t.title_id = sales.title_id
@@ -47,7 +50,10 @@ SELECT au.au_id AS Author_ID, au.au_lname AS Lname, au.au_fname AS Fname, SUM(t.
     LIMIT 3;
 
 # CHALLENGE_4
-SELECT au.au_id AS Author_ID, au.au_lname AS Lname, au.au_fname AS Fname, COALESCE(NULL, SUM(t.ytd_sales),0) AS TOTAL
+SELECT au.au_id AS Author_ID, 
+    au.au_lname AS Last_name, 
+    au.au_fname AS First_name, 
+    COALESCE(NULL, SUM(t.ytd_sales),0) AS TOTAL
 	FROM titles AS t 
 	LEFT JOIN sales
     ON  t.title_id = sales.title_id
@@ -59,6 +65,18 @@ SELECT au.au_id AS Author_ID, au.au_lname AS Lname, au.au_fname AS Fname, COALES
     ORDER BY sum(t.ytd_sales) DESC
 
 # BONUS
+
+SELECT au.au_id AS Author_ID, 
+    au.au_lname AS Last_name, 
+    au.au_fname AS First_name,
+    round(t.price*(t.royalty/100)*(tta.royaltyper/100),2)+round(t.advance*(tta.royaltyper/100),2) as Profit
+	FROM titles AS t 
+    INNER JOIN titleauthor AS tta
+    ON  t.title_id = tta.title_id
+    INNER JOIN authors AS au
+    ON tta.au_id = au.au_id
+    ORDER BY Profit DESC
+    LIMIT 3;
 
 
 
